3efd244a92536adc512a550e3325746b958fc0ac
Daniel Pham <hongsongp97@gmail.com>
2021-01-21 14:50:39 +0700

    First commit

---
+++ src/constants.ts
@@ -0,0 +1,54 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 1,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GÖRLI = 5,
+  KOVAN = 42
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+// TODO: Need to change to luaswap's factory address
+export const FACTORY_ADDRESS = '0x6203F5723758bFb32d139D39176047A214e3C486'
+
+// TODO: Need to change every code smartcontract updated
+export const INIT_CODE_HASH = '0x872fdad11a7bd0b3d4be7031fca9537ce03e1cc0aa4f232aea1a27271351a8a9'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _995 = JSBI.BigInt(995)
+export const _996 = JSBI.BigInt(996)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
