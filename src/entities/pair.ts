0d0536cffda51371a72ed36c27814dd324882256
thanhnv2 <thanhnv2@vnext.com.vn>
2021-06-01 16:12:55 +0700

    add get amount add one token

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -225,4 +225,19 @@ export class Pair {
       JSBI.divide(JSBI.multiply(liquidity.raw, this.reserveOf(token).raw), totalSupplyAdjusted.raw)
     )
   }
+
+  public getAmountsAddOneToken(
+    token: Token,
+    amountToken: TokenAmount
+  ) : [TokenAmount, TokenAmount] {
+    invariant(this.involvesToken(token), 'TOKEN')
+    invariant(this.involvesToken(amountToken.token), 'TOKEN')
+    const amountSwap = JSBI.divide(amountToken.raw, JSBI.BigInt('2'))
+    const tokenAmountSwap = new TokenAmount(token, amountSwap)
+    const [amountOut,] = this.getOutputAmount(tokenAmountSwap);
+    return [
+      amountOut, 
+      new TokenAmount(token, amountSwap)
+    ]
+  }
 }
