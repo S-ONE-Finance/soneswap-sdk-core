91c8af7bc80358aa6acebed9dd2474e6ecbf7b57
thanhnv2 <thanhnv2@vnext.com.vn>
2021-07-20 17:31:28 +0700

    covert jsbi to bignumber

--- src/entities/userInfo.ts
+++ src/entities/userInfo.ts
@@ -1,5 +1,5 @@
 import { UserInfoSushi as UserInfoInterface } from '../interfaces'
-import JSBI from 'jsbi'
+import BigNumber from 'bignumber.js'
 import { calculateAPY } from '../utils'
 import { PoolInfo } from './poolInfo'
 
@@ -13,48 +13,36 @@ export class UserInfo {
   }
 
   public getTotalStakedValueAfterStake(newValue: string): string {
-    const stakedValue = JSBI.BigInt(this.user.amount)
-    const newStakedValue = JSBI.BigInt(newValue)
-    return JSBI.add(stakedValue, newStakedValue).toString()
+    const stakedValue = new BigNumber(this.user.amount)
+    const newStakedValue = new BigNumber(newValue)
+    return stakedValue.plus(newStakedValue).toString()
   }
 
   public getEarnedRewardAfterStake(newValue: string, block: number): string {
-    const poolShare =
-      (Number(newValue) + Number(this.user.amount)) / (Number(newValue) + Number(this.poolInfo.pool.balance))
-    const rewardForUser = this.poolInfo.pool.rewardPerBlock * poolShare
+    const poolShare = (new BigNumber(newValue).plus(new BigNumber(this.user.amount))).div(new BigNumber(newValue).plus(new BigNumber(this.poolInfo.pool.balance)))
+    const rewardForUser = new BigNumber(this.poolInfo.pool.rewardPerBlock).div(poolShare)
     const multiplierYear = calculateAPY(this.poolInfo.pool.secondsPerBlock, block)
-    return (multiplierYear * rewardForUser).toString()
+    return (multiplierYear * rewardForUser.toNumber()).toString()
   }
 
-  // public getAPYAfterStake(newValue: string, block: number): string {
-  //   const poolShare =
-  //     Number(newValue) / (Number(newValue) + Number(this.poolInfo.pool.balance))
-  //   const roiPerBlock =
-  //     (this.poolInfo.pool.rewardPerBlock * this.poolInfo.pool.sushiPrice * poolShare) /
-  //     (Number(newValue) * this.poolInfo.pool.LPTokenPrice / 1e18)
-  //   const multiplierYear = calculateAPY(this.poolInfo.pool.secondsPerBlock, block)
-  //   return (multiplierYear * roiPerBlock).toString()
-  // }
-
   public getAPYAfterStake(newValue: string, block: number): string {
-    const poolShare =
-      (Number(newValue) + Number(this.user.amount)) / (Number(newValue) + Number(this.poolInfo.pool.balance))
-    const roiPerBlock =
-      (this.poolInfo.pool.rewardPerBlock * this.poolInfo.pool.sushiPrice * poolShare) /
-      (((Number(newValue) + Number(this.user.amount)) * this.poolInfo.pool.LPTokenPrice) / 1e18)
+    const poolShare = (new BigNumber(newValue).plus(new BigNumber(this.user.amount))).div(new BigNumber(newValue).plus(new BigNumber(this.poolInfo.pool.balance)))
+    const interestValue = new BigNumber(this.poolInfo.pool.rewardPerBlock * this.poolInfo.pool.sushiPrice * poolShare.toNumber())
+    const investValue = (new BigNumber(newValue).plus(new BigNumber(this.user.amount))).times(new BigNumber(this.poolInfo.pool.LPTokenPrice)).div(new BigNumber(1e18))
+    const roiPerBlock = interestValue.toNumber() / investValue.toNumber()
     const multiplierYear = calculateAPY(this.poolInfo.pool.secondsPerBlock, block)
     return (multiplierYear * roiPerBlock).toString()
   }
 
   public getTotalLPTokenAfterUnstake(currentLPToken: string, newValue: string): string {
-    const currentLPTokenValue = JSBI.BigInt(currentLPToken)
-    const newUnstakedValue = JSBI.BigInt(newValue)
-    return JSBI.subtract(currentLPTokenValue, newUnstakedValue).toString()
+    const currentLPTokenValue = new BigNumber(currentLPToken)
+    const newUnstakedValue = new BigNumber(newValue)
+    return currentLPTokenValue.plus(newUnstakedValue).toString()
   }
 
   public getRemainStakedValueAfterUnstake(newValue: string): string {
-    const stakedValue = JSBI.BigInt(this.user.amount)
-    const newUnstakedValue = JSBI.BigInt(newValue)
-    return JSBI.subtract(stakedValue, newUnstakedValue).toString()
+    const stakedValue = new BigNumber(this.user.amount)
+    const newUnstakedValue = new BigNumber(newValue)
+    return stakedValue.minus(newUnstakedValue).toString()
   }
 }
